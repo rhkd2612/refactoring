e29a115d3b360f53f723ffcbc23bcbb3ea8f2f59
leesangbin <mumomu@nm-fc.com>
2023-04-12 18:08:59 +0900

    refactor: thisAmount 메서드 분리

--- src/main/java/com/study/refactor/RefactorApplication.java
+++ src/main/java/com/study/refactor/RefactorApplication.java
@@ -13,25 +13,7 @@ public class RefactorApplication {
 
 		for(var perf : invoice.getPerformances()){
 			Play play = plays.get(perf.getPlayId());
-			var thisAmount = 0;
-
-			switch(play.getType()){
-				case "tragedy":
-					thisAmount = 40000;
-					if(perf.getAudience() > 30){
-						thisAmount += 1000 * (perf.getAudience() - 30);
-					}
-					break;
-				case "comedy":
-					thisAmount = 30000;
-					if(perf.getAudience() > 20){
-						thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-					}
-					thisAmount += 300 * perf.getAudience();
-					break;
-				default:
-					throw new Exception("알 수 없는 장르");
-			}
+			int thisAmount = calculateCurrentAmount(perf, play);
 
 			volumeCredits += Math.max(perf.getAudience() - 30, 0);
 			if("comedy".equals(play.getType())) volumeCredits += Math.floor(perf.getAudience() / 5);
@@ -42,6 +24,28 @@ public class RefactorApplication {
 
 		result += "총액: " + totalAmount + "원\n";
 		result += "적립 포인트: " + volumeCredits + "점\n";
+		return result;
+	}
+
+	private static int calculateCurrentAmount(Performance perf, Play play) throws Exception {
+		var result = 0;
+		switch(play.getType()){
+			case "tragedy":
+				result = 40000;
+				if(perf.getAudience() > 30){
+					result += 1000 * (perf.getAudience() - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if(perf.getAudience() > 20){
+					result += 10000 + 500 * (perf.getAudience() - 20);
+				}
+				result += 300 * perf.getAudience();
+				break;
+			default:
+				throw new Exception("알 수 없는 장르");
+		}
 		return result;
 	}
 
