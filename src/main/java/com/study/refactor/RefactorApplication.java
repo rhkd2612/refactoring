96fe7be7a15f6fc4772cf2a8f3a24ea9d01563f9
leesangbin <mumomu@nm-fc.com>
2023-05-17 18:29:28 +0900

    refactor: 필요없는 검증 구문 삭제

--- src/main/java/com/study/refactor/RefactorApplication.java
+++ src/main/java/com/study/refactor/RefactorApplication.java
@@ -6,53 +6,48 @@ import java.util.List;
 import com.study.refactor.playtype.PerformancePlayType;
 
 public class RefactorApplication {
-	public static String statement(Invoice invoice){
-		var result = new StringBuilder();
-		result.append(objectsToStrLine(result, "청구 내역 고객명 : ", invoice.getCustomerName()));
-		for(var perf : invoice.getPerformances()) {
-			Play play = PlayLoader.get(perf.getPlayId());
-			result.append(objectsToStrLine( ": ", play.calculateCurrentAmount(perf), "원, ", perf.getAudience(), "석"));
-		}
-		result.append(objectsToStrLine( "총액: ", invoice.calculateTotalAmount(), "원"));
-		result.append(objectsToStrLine( "적립 포인트: ", invoice.getVolumeCredits(), "점"));
-		return result.toString();
-	}
+    public static String statement(Invoice invoice) {
+        var result = new StringBuilder();
+        result.append(objectsToStrLine(result, "청구 내역 고객명 : ", invoice.getCustomerName()));
+        for(var perf : invoice.getPerformances()) {
+            Play play = PlayLoader.get(perf.getPlayId());
+            result.append(objectsToStrLine(": ", play.calculateCurrentAmount(perf), "원, ", perf.getAudience(), "석"));
+        }
+        result.append(objectsToStrLine("총액: ", invoice.calculateTotalAmount(), "원"));
+        result.append(objectsToStrLine("적립 포인트: ", invoice.getVolumeCredits(), "점"));
+        return result.toString();
+    }
 
-	private static String objectsToStrLine(Object... strings){
-		StringBuilder builder = new StringBuilder();
-		for(var string : strings)
-			builder.append(string);
-		builder.append('\n');
-		return builder.toString();
-	}
+    private static String objectsToStrLine(Object... strings) {
+        StringBuilder builder = new StringBuilder();
+        for(var string : strings)
+            builder.append(string);
+        builder.append('\n');
+        return builder.toString();
+    }
 
-	public static void main(String[] args){
-		if(invalidInput(args))
-			return;
-		String allPerformanceInfo = args[0];
-		String userName = args[1];
+    public static void main(String[] args) {
+        try {
+            String allPerformanceInfo = args[0];
+            String userName = args[1];
 
-		List<Performance> performances = new ArrayList<>();
-		List<String> performancesInfo = List.of(allPerformanceInfo.split(","));
-		performancesInfo.forEach(a -> {
-			try {
-				String[] split = a.split("/");
-				String tag = split[0], title = split[1];
-				PerformancePlayType type = PerformancePlayType.initPerformancePlayType(split[2]);
-				int audience = Integer.parseInt(split[3]);
+            List<Performance> performances = new ArrayList<>();
+            List<String> performancesInfo = List.of(allPerformanceInfo.split(","));
+            performancesInfo.forEach(a -> {
+                String[] split = a.split("/");
+                String tag = split[0], title = split[1];
+                PerformancePlayType type = PerformancePlayType.initPerformancePlayType(split[2]);
+                int audience = Integer.parseInt(split[3]);
 
-				performances.add(new Performance(tag, audience));
-				PlayLoader.putIfAbsent(tag, new Play(title, type));
-			} catch(ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
-				throw e;
-			}
-		});
+                performances.add(new Performance(tag, audience));
+                PlayLoader.putIfAbsent(tag, new Play(title, type));
 
-		Invoice invoices = new Invoice(userName, performances);
-		System.out.println(statement(invoices));
-	}
+            });
 
-	private static boolean invalidInput(String[] args) {
-		return args.length < 2;
-	}
+            Invoice invoices = new Invoice(userName, performances);
+            System.out.println(statement(invoices));
+        } catch(ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            throw e;
+        }
+    }
 }
