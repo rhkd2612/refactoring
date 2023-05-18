16f4d957048b1a2f6df6f6ee88b3d801d41c13c6
leesangbin <mumomu@nm-fc.com>
2023-05-18 18:49:36 +0900

    refactor: 내부 static PerformanceForm 클래스 추출

--- src/main/java/com/study/refactor/RefactorApplication.java
+++ src/main/java/com/study/refactor/RefactorApplication.java
@@ -5,29 +5,9 @@ import java.util.List;
 
 import com.study.refactor.exception.MyCustomException;
 import com.study.refactor.handler.MyCustomExceptionHandler;
-import com.study.refactor.playtype.PerformancePlayType;
+import com.study.refactor.performance.PerformanceForm;
 
 public class RefactorApplication {
-    private static class PerformanceForm{
-        public String tag;
-        public String title;
-        public PerformancePlayType type;
-        public Integer audience;
-        public PerformanceForm(String input) {
-            String[] split = input.split("/");
-            validatePerformanceInput(split);
-            this.tag = split[0];
-            this.title = split[1];
-            this.type = PerformancePlayType.initPerformancePlayType(split[2]);
-            this.audience = Integer.parseInt(split[3]);
-        }
-
-        private static void validatePerformanceInput(String[] split) {
-            if(split.length < 4)
-                throw new MyCustomException("Invalid Input Performance");
-        }
-    }
-
     public static void main(String[] args) {
         Thread.setDefaultUncaughtExceptionHandler(new MyCustomExceptionHandler());
         validateArguments(args);
---
+++ src/main/java/com/study/refactor/performance/PerformanceForm.java
@@ -0,0 +1,24 @@
+package com.study.refactor.performance;
+
+import com.study.refactor.exception.MyCustomException;
+import com.study.refactor.playtype.PerformancePlayType;
+
+public class PerformanceForm {
+    public String tag;
+    public String title;
+    public PerformancePlayType type;
+    public Integer audience;
+    public PerformanceForm(String input) {
+        String[] split = input.split("/");
+        validatePerformanceInput(split);
+        this.tag = split[0];
+        this.title = split[1];
+        this.type = PerformancePlayType.initPerformancePlayType(split[2]);
+        this.audience = Integer.parseInt(split[3]);
+    }
+
+    private static void validatePerformanceInput(String[] split) {
+        if(split.length < 4)
+            throw new MyCustomException("Invalid Input Performance");
+    }
+}
